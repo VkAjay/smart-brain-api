aad7bd46410ca6418cb04a899517d89159ac7e4c
VkAjay <59680173+VkAjay@users.noreply.github.com>
2020-08-26 19:18:52 +0530

    Add files via upload

---
+++ controllers/register.js
@@ -0,0 +1,35 @@
+const handleRegister = (db, bcrypt) => (req, res) => {
+	const { email, name, password } = req.body;
+	if(name && email && password) {
+		const hash = bcrypt.hashSync(password);
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+				.returning('*')
+				.insert({
+					email: loginEmail[0],
+					name: name,
+					doj: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);
+				})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		})
+		.catch(err => res.status(400).json('unable to register'));
+	} else {
+		res.status(400).json('Kindly fill all the details.');		
+	}
+}
+
+module.exports = {
+	handleRegister
+};
